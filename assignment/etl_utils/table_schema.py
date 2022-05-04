6c8bbb598b2dbabb38ed4b52778be27f543256b2
adrian-ja-projects <54493284+adrian-ja-projects@users.noreply.github.com>
2022-05-04 17:54:50 +0300

    schema to manipulate table schemas in spark read

---
+++ assignment/etl_utils/table_schema.py
@@ -0,0 +1,31 @@
+#Author: Adrian Jimenez 2022-05
+#develop for unity assignment
+
+import json
+
+from pathlib import Path
+from pyspark.sql.types import StructType, StructField
+
+class TableSchema:
+    """
+    Class to represent a spark table schema
+    @table_name: string
+    @schema_location_path: path where json file of the schema will be saved
+    @schema: schema taken from the df after applying the schema function
+    @absolute_schema_path
+    """
+    def __init__(self, table_name:str, schema_root_path:str):
+        self.table_name = table_name
+        self.schema_root_path = schema_root_path
+        self.absolute_schema_path = f'{self.schema_root_path}/schema_{table_name}.json'
+        
+    def save_schema_file_as_json(self, schema_in_json: json):
+        path = Path(self.schema_root_path)
+        path.mkdir(parents=True, exist_ok=True)
+        with open(self.absolute_schema_path, 'w') as p:
+            json.dump(schema_in_json, p)
+          
+    def load_schema_json(self):
+        with open(self.absolute_schema_path, 'r') as f:
+            schema_json = json.load(f)
+        return StructType.fromJson(json.loads(schema_json))
