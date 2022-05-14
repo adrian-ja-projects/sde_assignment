69bf06c2d5a5f72df664f07957648d387243198e
adrian-ja-projects <54493284+adrian-ja-projects@users.noreply.github.com>
2022-05-14 19:42:43 +0300

    add_spark_n_cassandra_conn_test

--- assignment/conftest.py
+++ assignment/conftest.py
@@ -1,6 +1,6 @@
-import pytest
-from delta import *
+from delta import configure_spark_with_delta_pip
 from pyspark.sql import SparkSession
+import pytest
 
 @pytest.fixture(scope='session')
 def spark_s():
