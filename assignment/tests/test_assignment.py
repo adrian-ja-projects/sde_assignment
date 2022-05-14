f921a165642f7ab1e7647e2c49aed78a47d01924
adrian-ja-projects <54493284+adrian-ja-projects@users.noreply.github.com>
2022-05-14 11:08:12 +0300

    spark running test

---
+++ assignment/tests/test_assignment.py
@@ -0,0 +1,12 @@
+from pyspark.sql import functions as F
+
+def test_spark_running(spark_s):
+    data = [("Hello", "World", "!")]
+    columns = ["First_Word", "Second_Word", "Exclamation"]
+    df = spark_s.createDataFrame(data, columns)
+    hello = df.select("First_Word").collect()[0][0]
+    world = df.select("Second_Word").collect()[0][0]
+    exclamation = df.select("Exclamation").collect()[0][0]
+    hello_world = f"{hello} {world} {exclamation}"
+    assert hello_world == "Hello World !"
+
